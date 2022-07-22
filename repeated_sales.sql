58b151c3af0f7060495c5b656f456342a2f55663
Danila <danila_sm@mail.ru>
2022-07-22 12:38:09 +0300

    added zeroing date, student name and country

--- repeated_sales.sql
+++ repeated_sales.sql
@@ -1,16 +1,17 @@
 with region as (
-	select 
-		lf.id,
-		cvr.country 
-	from amocrm.leads_fact lf
-	join amocrm.contacts_view_region cvr 
-		on lf.contacts_id = cvr.id
+ select 
+  lf.id,
+  cvr.country 
+ from amocrm.leads_fact lf
+ join amocrm.contacts_view_region cvr 
+  on lf.contacts_id = cvr.id
 )
 select distinct
  date(getdate()) as download_date,
  'https://kodland.amocrm.ru/leads/detail/' ||bs.amocrm_id as amocrm_link,
- --np.course,
- np.courseset,
+ bs.first_name || ' ' || bs.last_name as student_name,
+ np.course,
+ --np.courseset,
  bsg.title as "group",
  case
   when (case
@@ -37,7 +38,15 @@ select distinct
   end)
  , btlast."time")) as L4,
  wb.balance,
- r.country
+ r.country,
+ --bs.user_id student_id,
+ (case
+  when wb.balance = 0 then (select max(wt.transaction_date)::date from finance.wallet_transaction wt where wt."type" like '%subtract%' and wt.user_id = bs.user_id)
+  when wb.balance = -1 then (select date_add('week', -1, max(wt.transaction_date)::date)::date from finance.wallet_transaction wt where wt."type" like '%subtract%'  and wt.user_id = bs.user_id)
+  when wb.balance = 1 then (select date_add('week', 1, max(wt.transaction_date)::date)::date from finance.wallet_transaction wt where wt."type" like '%subtract%'  and wt.user_id = bs.user_id)
+  else null
+ end
+ ) zero_balance
 from
  kodland_shared.basics_student bs
 left join finance.new_payments np on
@@ -53,17 +62,17 @@ left join kodland_shared.basics_lesson bl on
 left join kodland_shared.basics_timetable btfirst on
  bsg.start_timeslot_id = btfirst.id
 left join region r 
-	on bs.amocrm_id = r.id
+ on bs.amocrm_id = r.id
 where
  bs.is_active = true
  and bs.deleted is null
  and bsg.title is not null 
  and (lower(bsg.title) not like '%%tc%%' or lower(bsg.title) like '%%scratch%%')
  and bsg.title not like '%%1-1%%'
- and lower(bsg.title) like 'online%%'
+ and lower(bsg.title) like 'itl%%'
  --and (lower(bsg.title) like 'esp%%' or lower(bsg.title) like 'chi%%' or lower(bsg.title) like 'arg%%')
  --and date(L4) between date(dateadd(day, 1, getdate())) and date(dateadd(week, 1, getdate()))
- and date(L4) between '2022-06-20' and '2022-06-26'
+ and date(L4) between '2022-07-18' and '2022-07-24'
  --and lower(bsg.title) like '%%scratch%%'
  --and m1l1 < '2022-06-01'
 order by L4
